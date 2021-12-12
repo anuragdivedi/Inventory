bd8d3419cc90176ca245af4071e364761ba23730
anuragdivedi <35894118+anuragdivedi@users.noreply.github.com>
2021-12-12 16:15:44 +0530

    Update latest.js.js

--- latest.js.js
+++ latest.js.js
@@ -1,109 +1,95 @@
 var express = require('express');
-// var bodyParser = require('body-parser');
-// var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 var app = express();
-// var oracledb = require('oracledb');
+var oracledb = require('oracledb');
 
-// oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-const mypw = "CHELYABINSK91Enriched"  // set mypw to the hr schema password
+const mypw = "CHELYABINSK91Enriched"
 
 
 app.get('/', function(req,res) {
-    console.log(__dirname)
-	res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/latest.html");
 });
 
-// oracledb.getConnection(
-//   {
-//     user: "inf10_cfg_sqlts", 
-//     password: mypw,
-//     connectString: "eipdb10dev.am.lilly.com:1533/dev_68"
-//   }, 
-//   function(err, connection) {
-//     if (err) {error = err; return;}
-    
-//     connection.execute('select DOMAIN_NAME from ENV_TABLE', [], function(err, result) {
-//       if (err) {error = err; return;}
+ 
 
-//       //user = result.rows[0][0];
-//       error = null;
-//       console.log(result);
-//       connection.close(function(err) {
-//         if (err) {console.log(err);}
-//       });
-//     })
-//   }
-// );
-app.get('/booklist', function(req,res){
-    return res.status(200).send('Pass') ;
-});
-app.get('/booklist/:id', function(req,res){
-    console.log(req.params.id);
-    return res.status(200).send(req.params.id) ;
-});
+ app.get('/booklist', function(req,res){
+  oracledb.getConnection(
+    {
+      user: "inf10_cfg_sqlts", 
+      password: mypw,
+      connectString: "eipdb10dev.am.lilly.com:1533/dev_68"
+    }, 
+    function(err, connection) {
+      if (err) {error = err; return;}
+     
+      connection.execute('select DOMAIN_NAME,HOST_NAME,OWNER_NAME,GW_PORT,DOMAIN_DB_ACCOUNT,CUSTODIAN_NAME,PRIMARY_CONTACT,COST_CENTER,VALUE from ENV_TABLE', [], function(err, result) {
+        if (err) {error = err; return;}
+        error = null;
+        console.log(result);
+        res.send(result);
+ 
+        connection.close(function(err) {
+          if (err) {console.log(err);}
+        });
+      })
+    }
+  );
 
-// app.get('/booklist', function(req,res){
-//   oracledb.getConnection(
-//     {
-//       user: "inf10_cfg_sqlts", 
-//       password: mypw,
-//       connectString: "eipdb10dev.am.lilly.com:1533/dev_68"
-//     }, 
-//     function(err, connection) {
-//       if (err) {error = err; return;}
-      
-//       connection.execute('select DOMAIN_NAME from ENV_TABLE', [], function(err, result) {
-//         if (err) {error = err; return;}
-  
-//         //user = result.rows[0][0];
-//         error = null;
-//         res.send(result);
-      
-//         console.log(result);
-//         connection.close(function(err) {
-//           if (err) {console.log(err);}
-//         });
-//       })
-//     }
-//   );
-// 	/*var db = mysql.createConnection( {
-// 		host: 'localhost',
-// 		user: 'root',
-// 		password: 'root',
-// 		database: 'inventory'
-		
-// 	});
-// 	db.connect(function(err) {  
-// 		if (err) throw err;  
-// 		console.log("Connected!"); 
-// 		db.query("SELECT * from mini", function(err, data) {
-// 			if(err) throw err;
-// 			res.send(data);
-// 			db.end();
-// 		}) 
-// 	  }); */
-// })
-	
-	
+ });
 
-	
+ app.get('/IOD', function(req,res){
+  oracledb.getConnection(
+    {
+      user: "inf10_cfg_sqlts", 
+      password: mypw,
+      connectString: "eipdb10dev.am.lilly.com:1533/dev_68"
+    }, 
+    function(err, connection) {
+      if (err) {error = err; return;}
+     
+      connection.execute('select ORGANIZATION_NAME from IOD_TABLE', [], function(err, result) {
+        if (err) {error = err; return;}
+        error = null;
+        console.log(result);
+        res.send(result);
+ 
+        connection.close(function(err) {
+          if (err) {console.log(err);}
+        });
+      })
+    }
+  );
 
+ });
 
-/*app.get('/userbooks', function(req,res) {
-	var db = mysql.createConnection( {
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'inventory'
-	});
-	db.connect();
-	db.query("SELECT * from dummy", function(err, data) {
-		if(err) throw err;
-		res.send(data);
-		db.end();
-	}) 
-}) */
+ app.get('/MDM', function(req,res){
+  oracledb.getConnection(
+    {
+      user: "inf10_cfg_sqlts", 
+      password: mypw,
+      connectString: "eipdb10dev.am.lilly.com:1533/dev_68"
+    }, 
+    function(err, connection) {
+      if (err) {error = err; return;}
+     
+      connection.execute('select ENV,HOSTNAME from MDM_TABLE', [], function(err, result) {
+        if (err) {error = err; return;}
+        error = null;
+        console.log(result);
+        res.send(result);
+ 
+        connection.close(function(err) {
+          if (err) {console.log(err);}
+        });
+      })
+    }
+  );
+
+ });
+
 
 	
 app.listen(3000, function() {
